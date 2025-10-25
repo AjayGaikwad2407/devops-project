a9633ef6b4546070b886215f55555503d5d5ae26
AjayGaikwad2407 <ajaygaikwad92@gmail.com>
2025-10-26 00:47:36 +0530

    update

--- infra/modules/acr/main.tf
+++ infra/modules/acr/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_container_registry" "aks-acr" {
-    name                = "${var.prefix}aksacr"
+    name                = "${var.prefix}aksacrkridhaimagerepo"
     location            = var.location
     resource_group_name = var.resource_group_name
     sku                 = "Standard"
