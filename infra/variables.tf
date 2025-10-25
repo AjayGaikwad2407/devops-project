1f9424ca2f62abcd2e368e1d509bc96b1dd33053
AjayGaikwad2407 <ajaygaikwad92@gmail.com>
2025-10-26 00:36:03 +0530

    update

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,21 +1,21 @@
 variable "resource_group_name" {
   description = "The name of the resource group"
   type        = string
-//  default = "prod-rg"
+  default = "prod-rg"
   
 }
 
 variable "location" {
   description = "The Azure region to deploy resources"
   type        = string
-//  default = "East US"
+  default = "East US"
   
 }
 
 variable "prefix" {
   description = "Prefix for naming resources"
   type        = string
-//  default = "prod" 
+  default = "prod" 
 }
 
 
