7ba82ed0102c25f2b8649601d1aec8bc45ff7b7c
AjayGaikwad2407 <ajaygaikwad92@gmail.com>
2025-10-25 23:29:07 +0530

    upate

--- infra/backend.tf
+++ infra/backend.tf
@@ -2,7 +2,6 @@ terraform {
   backend "azurerm" {
         resource_group_name = "tfstate-rg"
         storage_account_name = "kridhatfstatestorge"
-        key = "${var.prefix}.terraform.tfstate"
         container_name = "devops-project-tfstate"
   }
 }
